f252892f79322848ab7c51bc55486f1cf2341543
ABDUL MOIZ <babamoiz289@gmail.com>
2025-03-20 06:11:29 +0500

    Introduce a backend for BIOVA SURGICALS

---
+++ FrontEnd/src/components/FeaturedProducts.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const FeaturedProducts = () => {
+    return (
+        <div className='mt-[70px]'>
+            <p className='text-[#4F7D7B] text-center lato text-[20px]'>Highly Affordable Products</p>
+            <h1 className='text-[#00605f] playfair font-bold text-[46px] text-center tracking-[1.5px] mt-[20px]'>Featured Products</h1>
+        </div>
+    )
+}
+
+export default FeaturedProducts
--- FrontEnd/src/pages/Home.jsx
+++ FrontEnd/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import MainSection from '../components/MainSection'
 import BrandSection from '../components/BrandSection'
+import FeaturedProducts from '../components/FeaturedProducts'
 
 const Home = () => {
     return (
         <main>
             <MainSection />
             <BrandSection />
+            <FeaturedProducts />
         </main>
     )
 }
