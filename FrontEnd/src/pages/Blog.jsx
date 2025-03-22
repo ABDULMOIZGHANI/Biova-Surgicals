c53d39aa9723b720841132893b5e954eb81b3002
ABDUL MOIZ <babamoiz289@gmail.com>
2025-03-23 03:06:24 +0500

    Blog (Front-End)

--- FrontEnd/src/pages/Blog.jsx
+++ FrontEnd/src/pages/Blog.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import BlogCart from '../components/BlogCart'
 
 const Blog = () => {
     return (
-        <div>
-            BLOG
-        </div>
+        <section className="py-10 bg-white sm:py-16 lg:py-24">
+            <div className="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
+                <div className="flex items-end justify-between">
+                    <div className="flex-1 text-center lg:text-left">
+                        <h2 className="text-3xl font-bold leading-tight text-black sm:text-4xl lg:text-5xl playfair">Latest from blog</h2>
+                        <p className="max-w-xl mx-auto mt-4 text-base leading-relaxed text-gray-600 lg:mx-0 lato">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis.</p>
+                    </div>
+
+                </div>
+
+                <BlogCart />
+
+            </div>
+        </section>
+
     )
 }
 
